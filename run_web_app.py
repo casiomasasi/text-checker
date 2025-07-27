2e17b42b291409002438098550961379b9fba1ea
casiomasasi <casio.masasi@gmail.com>
2025-07-27 17:16:09 +0900

    Fix: Bind Flask to 0.0.0.0 for Render

--- run_web_app.py
+++ run_web_app.py
@@ -19,5 +19,6 @@ def upload():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
